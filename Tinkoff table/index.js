5eb620eb176c0f3e66e8d5000eff3f831907016e
Vladislav Krysin <krysin18@mail.ru>
2021-05-24 21:35:51 +0300

    feat: add some js star can change state checkboxes can be selected

--- Tinkoff table/index.js
+++ Tinkoff table/index.js
@@ -1,9 +1,69 @@
+const stars = document.querySelectorAll('.star');
 
-const str = 10;
-hello(str);
+console.log(stars);
+
+
+for (const star of stars) {
+  star.addEventListener('mouseover', () => {
+    if (star.src.match(/selectedStar/g)) {
+      return;
+    }
+    star.src = './img/table/tbody/hoverStar.svg';
+  });
+  star.addEventListener('mouseout', () => {
+    if (star.src.match(/selectedStar/g)) {
+      return;
+    }
+    star.src = './img/table/tbody/star.svg';
+  });
+  star.addEventListener('mousedown', () => {
+    (star.src.match(/selectedStar/g)) ?
+      star.src = './img/table/tbody/star.svg' :
+      star.src = './img/table/tbody/selectedStar.svg';
+  });
+}
+
+const mainCheckbox = document.querySelector('.mainCheckbox');
+const checkBoxes = document.querySelectorAll('.checkbox');
+
+mainCheckbox.addEventListener('mousedown', () => {
+  if (mainCheckbox.style.background.match(/CheckMark/g)) {
+    mainCheckbox.style.background = 'rgba(34, 62, 105, 0.2)';
+    setCheckboxesState(false);
+  } else {
+    mainCheckbox.style.background =
+        'url("./img/table/tbody/blueCheckMark.svg"), rgba(34, 62, 105, 0.2)';
+    setCheckboxesState(true);
+  }
+});
 /**
- * @param {string} str - message to display
+ * @param {boolean} state true - all select, false - all not select
  */
-function hello(str) {
-  console.log(params);
+function setCheckboxesState(state) {
+  if (state) {
+    setCheckboxesBackground(
+        'url("./img/table/tbody/blueCheckMark.svg"), rgba(34, 62, 105, 0.2)');
+  } else {
+    setCheckboxesBackground('rgba(34, 62, 105, 0.2)');
+  }
+}
+/**
+ *
+ * @param {string} background
+ */
+function setCheckboxesBackground(background) {
+  for (const checkbox of checkBoxes) {
+    checkbox.style.background = background;
+  }
+}
+
+for (const checkbox of checkBoxes) {
+  checkbox.addEventListener('mousedown', () => {
+    if (checkbox.style.background.match(/CheckMark/g)) {
+      checkbox.style.background = 'rgba(34, 62, 105, 0.2)';
+    } else {
+      checkbox.style.background =
+        'url("./img/table/tbody/blueCheckMark.svg"), rgba(34, 62, 105, 0.2)';
+    }
+  });
 }
